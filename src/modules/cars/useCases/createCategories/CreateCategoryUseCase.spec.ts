014ecee58203a6ede541ef405073c103351ca54f
ibarretom <igorbm.23@gmail.com>
2021-12-18 18:21:51 -0300

    teste do CreateCategoryUseCase

---
+++ src/modules/cars/useCases/createCategories/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,40 @@
+import { AppError } from "./../../../../errors/AppError";
+import { CategoriesRepositoryInMemory } from "../../repositories/categories/inMemory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+let createCategory: CreateCategoryUseCase;
+let createCategoryInMemory: CategoriesRepositoryInMemory
+
+describe("Create category", () => {
+  beforeEach(() => {
+    createCategoryInMemory = new CategoriesRepositoryInMemory();
+    createCategory = new CreateCategoryUseCase(createCategoryInMemory);
+  })
+  it("Should be able to create a new category", async () => {
+    const category = {
+      name: "Test",
+      description: "Test description"
+    }
+    await createCategory.execute(category);
+    const categoryInMemory = await createCategoryInMemory.findByName(category.name)
+
+    expect(categoryInMemory).toHaveProperty("id")
+  })
+
+  it("Should not be able to create a new category with same name", async () => {
+    expect(async () => {
+      const category = {
+        name: "Test",
+        description: "Test description"
+      }
+
+      await createCategory.execute(category);
+      await createCategory.execute(category);
+
+      const categoryInMemory = await createCategoryInMemory.findByName(category.name)
+      await createCategoryInMemory.findByName(category.name)
+    
+    }).rejects.toBeInstanceOf(AppError)
+
+  })
+})
