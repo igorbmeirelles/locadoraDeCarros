e30cde61657a6b2f0c93d570227d649baa204e6e
ibarretom <igorbm.23@gmail.com>
2021-12-20 20:36:46 -0300

    Refresh token use case

--- src/modules/accounts/infra/typeorm/repositories/token/UsersTokenRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/token/UsersTokenRepository.ts
@@ -9,12 +9,23 @@ class UsersTokenRepository implements IUsersTokenRepository {
   constructor() {
     this.repository = getRepository(UserTokens);
   }
+
   async create({ expires_date, refresh_token, user_id }: ICreateUserTokenDTO): Promise<UserTokens> {
     const userToken = await this.repository.create({ expires_date, refresh_token, user_id });
 
     await this.repository.save(userToken);
 
     return userToken;
+  }
+
+  async findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UserTokens> {
+    const tokens = await this.repository.findOne({ where: { user_id, refresh_token } });
+
+    return tokens;
+  }
+
+  async deleteById(id: string): Promise<void> {
+    await this.repository.delete(id)
   }
 
 }
---
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -0,0 +1,57 @@
+import { verify, sign } from 'jsonwebtoken';
+import { inject } from 'tsyringe';
+import auth from '../../../../config/auth';
+import { IDateProvider } from '../../../../shared/container/providers/dateProvider/IDateProvider';
+import { AppError } from '../../../../shared/errors/AppError';
+import { IUsersTokenRepository } from '../../repositories/token/IUsersTokenRepository';
+
+interface IRequest {
+  refresh_token: string;
+}
+
+interface IPayload {
+  sub: string;
+  email: string;
+}
+
+class RefreshTokenUseCase {
+  private userTokenRepository: IUsersTokenRepository
+  private dayjsDateProvider: IDateProvider
+  constructor(
+    @inject("UsersTokenRepository") userTokenRepository: IUsersTokenRepository,
+    @inject("DayjsDateProvider") dayjsDateProvider: IDateProvider
+  ) {
+    this.userTokenRepository = userTokenRepository;
+    this.dayjsDateProvider = dayjsDateProvider;
+  }
+  async execute({ refresh_token }: IRequest): Promise<string> {
+    const decode = verify(refresh_token, auth.secret_refresh_token) as IPayload;
+
+    if (!decode) {
+      throw new AppError('Invalid token');
+    }
+
+    const { sub: user_id, email } = decode
+
+    const userTokens = await this.userTokenRepository.findByUserIdAndRefreshToken(user_id, refresh_token);
+
+    if (!userTokens) {
+      throw new AppError("Refresh token not found", 401);
+    }
+
+    await this.userTokenRepository.deleteById(userTokens.id);
+
+    const expires_date = this.dayjsDateProvider.addDays(auth.expires_refresh_token_days);
+
+    const refresh_token_new = sign({ email }, auth.secret_refresh_token, {
+      subject: user_id,
+      expiresIn: auth.expires_in_refresh_token
+    })
+
+    const user_token = await this.userTokenRepository.create({ expires_date, user_id, refresh_token: refresh_token_new });
+    
+    return user_token.refresh_token;
+  }
+}
+
+export { RefreshTokenUseCase };
