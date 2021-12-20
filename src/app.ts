58ab9699d92da1a3ecb20c8e71746f27a14dce81
ibarretom <igorbm.23@gmail.com>
2021-12-19 22:08:33 -0300

    Adicao de dependencia supertest e refatoracao do server.ts

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import "reflect-metadata";
+
+import express, { NextFunction, Response, Request } from "express";
+import "express-async-errors";
+import { router } from "./shared/infra/http/routes"
+import createConnection from "./shared/infra/typeorm"
+
+import "./shared/container"
+
+import { AppError } from "./shared/errors/AppError";
+
+const app = express();
+
+createConnection()
+
+app.use(express.json())
+app.use(router)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: "error",
+      message: err.message
+    })
+  }
+  return res.status(500).json({ message: err.message })
+})
+
+export { app }
