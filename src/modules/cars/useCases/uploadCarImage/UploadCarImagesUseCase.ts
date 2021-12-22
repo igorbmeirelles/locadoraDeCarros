2dfc78462fff3fb3e3ae83371cd617ecc1388004
ibarretom <igorbm.23@gmail.com>
2021-12-22 16:55:44 -0300

    Corrigindo upload de imagem de carros

--- src/modules/cars/useCases/uploadCarImage/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImagesUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { IStorageProvider } from "../../../../shared/container/providers/storageProvider/IStorageProvider";
 import { ICarsImagesRepository } from "../../repositories/cars/ICarsImagesRepository";
 
 interface IRequest {
@@ -8,17 +9,21 @@ interface IRequest {
 
 @injectable()
 class UploadCarImagesUseCase {
-  private carsImageRepository: ICarsImagesRepository
-  constructor(@inject("CarsImagesRepository") carsImageRepository: ICarsImagesRepository) {
-    this.carsImageRepository = carsImageRepository
+  private carsImageRepository: ICarsImagesRepository;
+  private storageProvider: IStorageProvider;
+  constructor(
+    @inject("CarsImagesRepository") carsImageRepository: ICarsImagesRepository,
+    @inject("StorageProvider") storageProvider: IStorageProvider
+  ) {
+    this.carsImageRepository = carsImageRepository;
+    this.storageProvider = storageProvider;
   }
 
   async execute({ car_id, images_name }: IRequest): Promise<void> {
-    
     images_name.map(async (image_name) => {
-      await this.carsImageRepository.create({ car_id, image_name })
-    })
-
+      await this.carsImageRepository.create({ car_id, image_name });
+      await this.storageProvider.save(image_name, "cars");
+    });
   }
 }
 
