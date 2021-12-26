f6df2c38e1a746004919ac3bb954b6154749f593
ibarretom <igorbm.23@gmail.com>
2021-12-26 14:31:34 -0300

    Sentry error handler

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,9 @@ import cors from "cors";
 import rateLimiter from "./shared/infra/http/middleware/RateLimiter";
 import "./shared/container";
 
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+
 import { AppError } from "./shared/errors/AppError";
 import upload from "./config/upload";
 
@@ -16,12 +19,29 @@ createConnection();
 const app = express();
 
 app.use(rateLimiter)
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(cors());
 app.use(express.json());
+
 app.use(router);
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof AppError) {
